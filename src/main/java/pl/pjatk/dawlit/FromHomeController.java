d043b99277aece801eb3a0ae9fe0154ad48c5df5
s27814-pj <s27814@pjwstk.edu.pl>
2024-04-06 11:38:48 +0200

    2zjazd

---
+++ src/main/java/pl/pjatk/dawlit/FromHomeController.java
@@ -0,0 +1,37 @@
+package pl.pjatk.dawlit;
+
+import org.apache.coyote.Response;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("/homework")
+public class FromHomeController {
+
+    @GetMapping({"/here/{any}", "/here"})
+    public ResponseEntity<String> hello(@PathVariable(required = false) String any, @RequestParam(required = false) String reqParam){
+        if (any!=null) return ResponseEntity.ok(any);
+        if (reqParam!=null) return ResponseEntity.ok(reqParam);
+        return ResponseEntity.ok("Hello world");
+    }
+
+    @PutMapping("/put/{name}")
+    public ResponseEntity<Car> putterCar(@PathVariable(required = true) String name, @RequestBody Car car){
+        Car car1 = new Car(car.getMakeYear(), name,car.getaBoolean());
+        return new ResponseEntity<>(car1, HttpStatus.OK);
+    }
+
+    @PostMapping("/post")
+    public ResponseEntity<Car> posterCar(@RequestBody Car car){
+        return new ResponseEntity<>(car, HttpStatus.OK);
+    }
+    @DeleteMapping("/delete/{any}")
+        public HttpStatus delete(@PathVariable(required = false) String any){
+            return HttpStatus.OK;
+        }
+
+
+
+}
---
+++ src/main/java/pl/pjatk/dawlit/TestController.java
@@ -0,0 +1,36 @@
+package pl.pjatk.dawlit;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("/test")
+public class TestController {
+
+
+    @GetMapping("/hello")
+    public ResponseEntity<String> helloParam(@RequestParam String reqParam){
+        return ResponseEntity.ok(reqParam);
+    }
+    /*@GetMapping("/hello")
+    public ResponseEntity<String> hello(){
+        return ResponseEntity.ok("Hello world");
+    }*/
+
+    @GetMapping("/model")
+    public ResponseEntity<Car> model(){
+        return new ResponseEntity<>(new Car(), HttpStatus.OK);
+    }
+
+    @PostMapping("/model")
+    public ResponseEntity<Car> modelPost(@RequestBody Car car){
+    return new ResponseEntity<>(car, HttpStatus.OK);
+    }
+
+    @GetMapping("/hello/{someValue}")
+    public ResponseEntity<String> helloValue(@PathVariable String someValue){
+        return ResponseEntity.ok(someValue);
+    }
+}
